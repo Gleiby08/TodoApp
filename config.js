c1e321fa8c9d2b064bce89f8d6cb0d050bf6a357
Gleiby_2020 <63572757+Gleiby08@users.noreply.github.com>
2025-07-28 17:40:17 -0400

    Add files via upload
    Fixed link prod

--- config.js
+++ config.js
@@ -1,8 +1,9 @@
-// Configuración de la URL de la página según el entorno
 const PAGE_URL = process.env.NODE_ENV === 'production'
-    ? 'placeholder'
-    : 'http://localhost:3000'
+  ? 'https://todoapp-xg5t.onrender.com'
+  : 'http://localhost:5500';
 
-    
-// Configuración de la URL de la página
-module.exports = { PAGE_URL };
+const MONGO_URI = process.env.NODE_ENV === 'production'
+  ? process.env.MONGO_URI_PROD
+  : process.env.MONGO_URI_TEST;
+
+module.exports = { PAGE_URL, MONGO_URI };
