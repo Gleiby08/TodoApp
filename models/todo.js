ed2dc5729f3249be77867f099b380f62f6a93a0c
Gleiby_2020 <63572757+Gleiby08@users.noreply.github.com>
2025-07-28 13:22:38 -0400

    Add files via upload

---
+++ models/todo.js
@@ -0,0 +1,31 @@
+// Importar las dependencias necesarias
+const mongoose = require('mongoose');
+
+
+// Este esquema define la estructura de los documentos de usuario en la base de datos
+// new mongoose.Schema crea un nuevo esquema de Mongoose
+const todoSchema = new mongoose.Schema({
+    text: String,
+    checked: Boolean,
+    user: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+        required: true
+    }
+});
+
+// Método para verificar el token de acceso
+todoSchema.set('toJSON', {
+    transform: (document, returnedObject) => {
+        returnedObject.id = returnedObject._id.toString();
+        delete returnedObject._id;
+        delete returnedObject.__v;
+       
+    }
+});
+
+// Crear el modelo de usuario a partir del esquema
+const Todo = mongoose.model('Todo', todoSchema);
+
+// Exportar el modelo de usuario
+module.exports = Todo;
--- models/user.js
+++ models/user.js
@@ -2,7 +2,9 @@
 const { verify } = require('jsonwebtoken');
 const mongoose = require('mongoose');
 
-// Definir el esquema del modelo de usuario
+
+// Este esquema define la estructura de los documentos de usuario en la base de datos
+// new mongoose.Schema crea un nuevo esquema de Mongoose
 const userSchema = new mongoose.Schema({
     name: String,
     email: String,
@@ -11,6 +13,11 @@ const userSchema = new mongoose.Schema({
         type: Boolean,
         default: false
     },
+    todos: [{
+            type: mongoose.Schema.Types.ObjectId,
+            ref: 'Todo',
+            required: true
+        }]
 });
 
 // Método para verificar el token de acceso
